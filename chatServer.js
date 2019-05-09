9a6119c465f1f1fb937f741c98b80271b11bc49c
Halo0070 <lee998598@gmail.com>
2019-05-09 16:58:48 +0900

    Update chatServer.js

--- chatServer.js
+++ chatServer.js
@@ -19,5 +19,20 @@ var chat=io.of('/chat').on('connection', function(socket) {
 
         // room에 join함.
         socket.join(room);
+
+        let msg=data.msg
+        let return_date ={
+            type: type,
+            name: name,
+            msg: msg,
+            date: getTimeStamp()
+        };
+        // room에 join되어 있는 client에게 message를 전송함.
+        chat.to(room).emit('chat message', return_date);
     });
 });
+
+function getTimeStamp(addday) {
+    var d=new Date();
+    return d.getFullYear() + '-' + (Number(d.getMonth())+1) + '-' + d.getDate() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
+}
