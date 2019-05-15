859ce858953dc6d7220de0b160542f9cf4dc18bd
Halo0070 <lee998598@gmail.com>
2019-05-15 09:03:36 +0900

    Update index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+var express=require('express');
+var bodyParser=require('body-parser');
+
+var app=express();
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/', function (req, res) {
+    res.send("Hello!");
+});
+
+app.get('/write', function (req, res) {
+    res.sendFile(__dirname+"/write.html");
+});
+
+app.post('/write', function (req, res) {
+    res.send(req.body);
+    let content=req.body;
+    let doc=JSON.parse(JSON.stringify(content));
+    let result_date = {
+        title: doc.title + 'ㅋㅋ',
+        content: doc.content + 'ㅎㅎ'
+    };
+    res.jsonp(result_date);
+    res.end();
+});
