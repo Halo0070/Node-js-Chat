cc00f6ad229200465051161a431c16179c59c4a0
Halo0070 <lee998598@gmail.com>
2019-06-27 20:56:02 +0900

    Update chatServer.js

--- chatServer.js
+++ chatServer.js
@@ -3,12 +3,21 @@ var server=require('http').createServer(app);
 // http server를 socket.io server로 upgrade함.
 var io=require('socket.io')(server);
 var ejs=require('ejs');
+var bodyParser=require('body-parser');
 var NickName="";
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 // localhost:3003으로 server에 접속하면 client로 chatFront.html을 전송함.
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/chatFront.html');
 });
+
+app.post('/', function(req, res){
+    console.log('POST');
+})
 
 // namespace /chat에 접속.
 var chat=io.of('/chat').on('connection', function(socket) {
